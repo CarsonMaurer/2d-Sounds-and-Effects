b7bd884992f84ebb89ec8495dc5eeab589657568
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-10-12 11:02:16 -0700

    added graphics

--- 2D Sounds And Effects/Assets/Scripts/SpawnManager.cs
+++ 2D Sounds And Effects/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 _spawnPos = new Vector3(15, -2.61f, 0);
+    private Vector3 _spawnPos = new Vector3(15, -2.78f, 0);
     private float _startDelay = 2;
     private float _repeatRate = 1;
     // Start is called before the first frame update
