81cac5a5372596e56dafe96a85faa098d5cc7c70
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-10-17 11:32:10 -0700

    updated

--- 2D Sounds And Effects/Assets/Scripts/PlayerController.cs
+++ 2D Sounds And Effects/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 10;
-    public bool isOnGround;
+    public bool isOnGround = false;
+
+    public bool isGameover = false;
 
     private Rigidbody2D _playerRB;
+    private Animator _playerAnim;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerRB = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameover)
         {
             _playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
+            _playerAnim.SetBool("IsOnGround", false);
         }
     }
-    private void OnCollisionEnter2D(Collision2D other) 
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            _playerAnim.SetBool("IsOnGround", true);
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            isGameover = true;
         }
     }
 }
--- 2D Sounds And Effects/Assets/Scripts/SpawnManager.cs
+++ 2D Sounds And Effects/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 _spawnPos = new Vector3(15, -2.78f, 0);
     private float _startDelay = 2;
-    private float _repeatRate = 1;
+    private float _repeatRate = 2;
     // Start is called before the first frame update
     void Start()
     {
